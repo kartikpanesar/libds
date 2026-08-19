1b15673d111a75a88a3dd795bd68512253aceec4
Kartik Panesar <kartikpanesar@gmail.com>
2026-08-19 23:35:38 +0530

    correcting a mistake

--- include/map.h
+++ include/map.h
@@ -8,7 +8,7 @@ typedef struct Map map;
 map *map_init();
 
 void map_add(map *m, const char* key, const void* value, size_t element_size);
-void map_get(map *m, const char* key);
+void *map_get(map *m, const char* key);
 
 void map_free(map *m);
 
